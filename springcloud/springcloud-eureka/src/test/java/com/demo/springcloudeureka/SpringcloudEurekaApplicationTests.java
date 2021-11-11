84d14c39242bebf01c5b5f561e458b73d5d7f155
wangbs <wangbaishun@gwssi.com.cn>
2021-11-11 15:04:11 +0800

    feat: 初始导入

---
+++ springcloud/springcloud-eureka/src/test/java/com/demo/springcloudeureka/SpringcloudEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.springcloudeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
